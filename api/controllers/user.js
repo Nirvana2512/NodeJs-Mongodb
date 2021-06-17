4670c522c6c9d4df4733f0d8165f15eec35eedc0
Nirvana2512 <63945015+Nirvana2512@users.noreply.github.com>
2021-06-17 17:29:16 +0530

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,21 +1,113 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+
+createHash = (password) => {
+    return new Promise((resolved, reject) => {
+        try {
+            bcrypt.hash(password, 10, (err, hash) => {
+                if (hash) {
+                    resolved(hash);
+                }
+                reject(err);
+
+            });
+        } catch (e) {
+            console.log(e);
+            reject(err);
+        }
+    })
+}
+verifyPassword = (newPassword, hash) => {
+    return new Promise((resolved, reject) => {
+        try {
+            bcrypt.compare(newPassword, hash, (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                resolved(result);
+
+            });
+        } catch (e) {
+            console.log(e);
+            reject(err);
+        }
+    })
+}
 
 module.exports.register = async (req, res) => {
     try {
-        const user ={
+        const password = await createHash(req.body.password);
+        console.log('password', password);
+        const user = {
             name: req.body.name,
-            username: req.body.username
+            username: req.body.username,
+            password: password
         }
         const userData = await User.create(user);
+        console.log('userData', userData);
+        delete userData._doc.password;
         res.json(
             {
                 message: 'successful',
                 playload: userData
             }
-        );
-       
-    } catch(e){
-        console.log(e);
-        res.json(e);
-    }
+        );    
+    } catch (e) {
+                console.log(e);
+                res.json({
+                    error: e.message
+                });
+            }
+        }
+ module.exports.login = async (req, res) => {
+        try {
+                const userData = await User.findOne(req.body.username);
+                if(!userData){
+                    res.json(
+                        {
+                            message: 'Invalid Cridentials',
+                        }
+                    );
+                }
+                const isPasswordMatched = await verifyPassword(req.body.password, userData.password);
+                delete userData._doc.password;
+
+                if(isPasswordMatched) {
+                    res.json(
+                        {
+                            message: 'successful',
+                            playload: userData
+                        }
+                    );
+            } else {
+                res.json(
+                    {
+                        message: 'Invalid Cridentials',
+                    }
+                );
+            }
+        } catch (e) {
+                console.log(e);
+                res.json({
+                    error: e.message
+                });
+            }
+        }
+
+ module.exports.get = async (req, res) => {
+            try {
+                const userData = await User.findById(req.params.id);
+                res.json(
+                    {
+                        message: 'successful',
+                        playload: userData
+                    }
+                );
+            } catch (e) {
+                console.log(e);
+                res.json({
+                    error: e.message
+                });
+            }
 }
