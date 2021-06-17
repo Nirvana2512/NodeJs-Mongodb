ed23760acef27dc5b2c1e30863332a6ddf9b879a
Nirvana2512 <63945015+Nirvana2512@users.noreply.github.com>
2021-06-17 17:28:09 +0530

    Update user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,11 +2,14 @@ let express = require('express');
 const router = express.Router();
 
 const {
-    register
+    register,
+    get
 } = require('../controllers/user')
 
 const userRoute = (expressApp) => {
     router.post('/', register);
+    router.post('/login', login);
+    router.get('/:id', get);
     expressApp.use('/api/user', router);
 }
 
